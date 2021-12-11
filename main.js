f785e1fdfe5069e925ecdeb9573535110942b1e5
Coder962 <76476530+Coder962@users.noreply.github.com>
2021-12-11 20:52:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-alive = "";
-candyland= "";
+song1 = "";
+song2= "";
 song = "";
+song1_status = "";
+song2_status = "";
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 function preload() {
-    song = loadSound("Alive.mp3");
-    song = loadSound("Candyland.mp3");
+    song1 = loadSound("Alive.mp3");
+    song2 = loadSound("Candyland.mp3");
 }
 function setup() {
     canvas = createCanvas(300,300,80,180);
@@ -19,6 +23,7 @@ function setup() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = "+ leftWristX +" leftWristY = "+ leftWristY);
@@ -33,4 +38,32 @@ function modelLoaded() {
 }
 function draw() {
     image(video,0,0,300,300);
+    	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+    if(scoreRightWrist > 0.2) {
+        circle(rightWristX,rightWristY,20);
+        song2.stop();
+        if(song1_status == false) {
+            song1.play();
+            document.getElementById("song").innerHTML = "Playing - Alive ";
+        }
+    }
+    if(scoreLeftWrist > 0.2) {
+        circle(leftWristX,leftWristY,20);
+        song1.stop();
+        if(song2_status == false) {
+            song2.play();
+            document.getElementById("song").innerHTML = "Playing - Candyland ";
+        }
+    }
+}
+function play() {
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
 }
