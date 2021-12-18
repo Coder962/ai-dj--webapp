77264ca49f120089220473b52381da5ad2abc5d2
Coder962 <76476530+Coder962@users.noreply.github.com>
2021-12-18 14:52:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 song1 = "";
-song2= "";
-song = "";
+song2 = "";
+
 song1_status = "";
 song2_status = "";
+
 scoreRightWrist = 0;
 scoreLeftWrist = 0;
-leftWristX = 0;
-leftWristY = 0;
+
 rightWristX = 0;
 rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
 function preload() {
     song1 = loadSound("Alive.mp3");
     song2 = loadSound("Candyland.mp3");
@@ -20,18 +23,26 @@ function setup() {
     video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
 }
-function gotPoses(results) {
-    if(results.length > 0) {
-        console.log(results);
-        scoreLeftWrist = results[0].pose.keypoints[9].score;
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = "+ leftWristX +" leftWristY = "+ leftWristY);
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = "+ rightWristX +" rightWristY = "+ rightWristY);
-    }
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
 }
+
 function modelLoaded() {
     console.log('PoseNet is Initialized');
     poseNet.on('pose', gotPoses);
